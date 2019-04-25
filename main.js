069429d5078e76ef9e8b393405479b08077a06ed
tjr <2871779035@qq.com>
2019-04-25 10:48:52 +0800

    autoslide

--- main.js
+++ main.js
@@ -2,14 +2,33 @@ var allLis = $(".menu > ul > li")
 var allDiv1 = $(".menu > ul > li > .div1")
 var allDiv2 = $(".menu > ul > li > .div2")
 var images = $(".images")
-for(var i=0;i<allLis.length;i++){
+var n = 0
+var timer = setInterval(function(){
+    $(images).css({
+        transform:'translateX('+ n%4* -920 +'px)'
+    })
+    n++
+    console.log('定时器')
+},2000)
+for(let i=0;i<allLis.length;i++){
     $(allLis[i]).on('click',function(e){
         let _this = e.currentTarget
         let index = $(_this).index()
         $(images).css({
             transform:'translateX('+index* -920+'px)'
         })
-        $(_this).addClass('active').siblings('.active').removeClass('active')
-        
+        $(_this).addClass('active').siblings('.active').removeClass('active') 
+        n = index
     })
 }
+$('#window').on('mouseenter',function(){
+    window.clearInterval(timer)
+})
+$('#window').on('mouseleave',function(){
+    timer = setInterval(function(){
+        $(images).css({
+            transform:'translateX('+ n%4* -920 +'px)'
+        })
+        n++
+    },2000)
+})
