6842547bb249001c26adddcc8ab34ce4c25c1fb7
tjr <2871779035@qq.com>
2019-04-25 11:05:43 +0800

    优化

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 var allLis = $(".menu > ul > li")
-var allDiv1 = $(".menu > ul > li > .div1")
-var allDiv2 = $(".menu > ul > li > .div2")
 var images = $(".images")
 var n = 0
 var timer = setInterval(function(){
@@ -8,14 +6,13 @@ var timer = setInterval(function(){
         transform:'translateX('+ n%4* -920 +'px)'
     })
     n++
-    console.log('定时器')
 },2000)
 for(let i=0;i<allLis.length;i++){
     $(allLis[i]).on('click',function(e){
         let _this = e.currentTarget
         let index = $(_this).index()
         $(images).css({
-            transform:'translateX('+index* -920+'px)'
+            transform:'translateX('+index* -920 +'px)'
         })
         $(_this).addClass('active').siblings('.active').removeClass('active') 
         n = index
